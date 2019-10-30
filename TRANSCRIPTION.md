a43dd758113c585d8266e2cb4ef3cabe0f88dd81
SimonGreenhill <simon@simon.net.nz>
2019-10-30 22:08:55 +0100

    update to 2.0

--- TRANSCRIPTION.md
+++ TRANSCRIPTION.md
@@ -5,49 +5,54 @@
 
 | Segment | Occurrence | BIPA | CLTS SoundClass |
 |:----------|-------------:|:-------|:------------------|
-| a | 4932 | ✓ | ✓ |
-| i | 2370 | ✓ | ✓ |
-| u | 1669 | ✓ | ✓ |
-| t | 1448 | ✓ | ✓ |
-| k | 1387 | ✓ | ✓ |
-| e | 1268 | ✓ | ✓ |
-| l | 1228 | ✓ | ✓ |
-| o | 1153 | ✓ | ✓ |
-| r | 1143 | ✓ | ✓ |
-| n | 977 | ✓ | ✓ |
-| m | 945 | ✓ | ✓ |
-| ŋ | 798 | ✓ | ✓ |
-| b | 791 | ✓ | ✓ |
-| + | 687 | ✓ | ✓ |
-| s | 665 | ✓ | ✓ |
-| w | 590 | ✓ | ✓ |
-| g | 574 | ✓ | ✓ |
-| d | 538 | ✓ | ✓ |
-| p | 486 | ✓ | ✓ |
-| ʔ | 402 | ✓ | ✓ |
-| h | 322 | ✓ | ✓ |
-| j | 298 | ✓ | ✓ |
-| ə | 208 | ✓ | ✓ |
-| ɛ | 191 | ✓ | ✓ |
-| q | 150 | ✓ | ✓ |
-| f | 148 | ✓ | ✓ |
-| ħ | 120 | ✓ | ✓ |
-| ɑ | 78 | ✓ | ✓ |
-| dʒ | 71 | ✓ | ✓ |
-| v | 63 | ✓ | ✓ |
-| ʊ | 55 | ✓ | ✓ |
-| ɪ | 40 | ✓ | ✓ |
-| tʃ | 35 | ✓ | ✓ |
-| aː | 33 | ✓ | ✓ |
+| a | 5153 | ✓ | ✓ |
+| i | 2512 | ✓ | ✓ |
+| u | 1746 | ✓ | ✓ |
+| t | 1576 | ✓ | ✓ |
+| k | 1417 | ✓ | ✓ |
+| e | 1400 | ✓ | ✓ |
+| l | 1359 | ✓ | ✓ |
+| o | 1352 | ✓ | ✓ |
+| r | 1225 | ✓ | ✓ |
+| n | 1112 | ✓ | ✓ |
+| + | 1019 | ✓ | ✓ |
+| m | 1018 | ✓ | ✓ |
+| b | 828 | ✓ | ✓ |
+| ŋ | 828 | ✓ | ✓ |
+| s | 738 | ✓ | ✓ |
+| w | 640 | ✓ | ✓ |
+| ʔ | 637 | ✓ | ✓ |
+| g | 622 | ✓ | ✓ |
+| d | 613 | ✓ | ✓ |
+| p | 565 | ✓ | ✓ |
+| h | 382 | ✓ | ✓ |
+| j | 327 | ✓ | ✓ |
+| ə | 209 | ✓ | ✓ |
+| ɛ | 193 | ✓ | ✓ |
+| f | 173 | ✓ | ✓ |
+| q | 153 | ✓ | ✓ |
+| ħ | 123 | ✓ | ✓ |
+| ɑ | 81 | ✓ | ✓ |
+| v | 73 | ✓ | ✓ |
+| dʒ | 72 | ✓ | ✓ |
+| tʃ | 64 | ✓ | ✓ |
+| <<’>> | 58 | ? | ? |
+| ʊ | 58 | ✓ | ✓ |
+| <<‘>> | 54 | ? | ? |
+| ɪ | 43 | ✓ | ✓ |
+| aː | 34 | ✓ | ✓ |
 | ɔ | 15 | ✓ | ✓ |
+| V/ǝ | 9 | ✓ | ✓ |
 | iː | 9 | ✓ | ✓ |
 | O/ɔ | 8 | ✓ | ✓ |
-| V/ǝ | 6 | ✓ | ✓ |
+| I/ɪ | 6 | ✓ | ✓ |
 | uː | 6 | ✓ | ✓ |
+| <<.>> | 3 | ? | ? |
 | oː | 3 | ✓ | ✓ |
 | ǝ | 3 | ✓ | ✓ |
 | ɣ | 3 | ✓ | ✓ |
-| I/ɪ | 2 | ✓ | ✓ |
+| <<2>> | 1 | ? | ? |
+| <<z>> | 1 | ? | ? |
 | aˑ | 1 | ✓ | ✓ |
 | eː | 1 | ✓ | ✓ |
 | µ/u | 1 | ✓ | ✓ |
@@ -55,7 +60,7 @@
 | ɒ | 1 | ✓ | ✓ |
 | ẽ | 1 | ✓ | ✓ |
 
-(49 rows)
+(54 rows)
 
 
 
@@ -71,8 +76,71 @@
 ## Words with invalid segments (up to 100 only)
 
 | ID | LANGUAGE | CONCEPT | FORM | SEGMENTS |
-||
+|:--------------------------------|:----------------|:-----------------------|:-----------------------------------------|:----------------------------------------------------------------------------------------------------------|
+| abui-10_arrow-1 | abui | 10_arrow | pulang ‘small arrow’ | p u l a ŋ + <s> <<‘>> </s> s m a l l + a r r o w <s> <<’>> </s> |
+| abui-10_arrow-2 | abui | 10_arrow | kaw ‘large arrow’ | k a w + <s> <<‘>> </s> l a r g e + a r r o w <s> <<’>> </s> |
+| abui-10_arrow-3 | abui | 10_arrow | kafu ‘large arrow’ | k a f u + <s> <<‘>> </s> l a r g e + a r r o w <s> <<’>> </s> |
+| abui-146_help-1 | abui | 146_help | netulung ‘bantu saya’ | n e t u l u ŋ + <s> <<‘>> </s> b a n t u + s a j a <s> <<’>> </s> |
+| abui-161_lake-1 | abui | 161_lake | wa: takata ‘lake that dries out quickly’ | w aː + t a k a t a + <s> <<‘>> </s> l a k e + t h a t + d r i e s + o u t + q u i tʃ k l j <s> <<’>> </s> |
+| abui-167_lietelluntruth-2 | abui | 167_lietelluntruth | edo okoli ‘you lie’ | e d o + o k o l i + <s> <<‘>> </s> j o u + l i e <s> <<’>> </s> |
+| abui-169_light-3 | abui | 169_light | weria ‘midday’ | w e r i a + <s> <<‘>> </s> m i d d a j <s> <<’>> </s> |
+| abui-191_mother-1 | abui | 191_mother | hea ‘his | h e a + <s> <<‘>> </s> h i s |
+| abui-191_mother-2 | abui | 191_mother | her mother’ | h e r + m o t h e r <s> <<’>> </s> |
+| abui-191_mother-3 | abui | 191_mother | nia ‘my mother’ | n i a + <s> <<‘>> </s> m j + m o t h e r <s> <<’>> </s> |
+| abui-215_person-2 | abui | 215_person | amakang ‘human being’ | a m a k a ŋ + <s> <<‘>> </s> h u m a n + b e i ŋ <s> <<’>> </s> |
+| abui-21_bean-1 | abui | 21_bean | takoi ‘large bean’ | t a k o i + <s> <<‘>> </s> l a r g e + b e a n <s> <<’>> </s> |
+| abui-21_bean-2 | abui | 21_bean | luong ‘small bean’ | l u o ŋ + <s> <<‘>> </s> s m a l l + b e a n <s> <<’>> </s> |
+| abui-21_bean-3 | abui | 21_bean | nai ‘ko.bean’ | n a i + <s> <<‘>> </s> k o <s> <<.>> </s> b e a n <s> <<’>> </s> |
+| abui-312_tobatheachild-1 | abui | 312_tobatheachild | Markus ha-wel ‘wash Markus’ | m a r k u s + h a w e l + <s> <<‘>> </s> w a s h + m a r k u s <s> <<’>> </s> |
+| abui-333_togive-2 | abui | 333_togive | minede ‘give me’ | m i n e d e + <s> <<‘>> </s> g i v e + m e <s> <<’>> </s> |
+| abui-336_tohit-2 | abui | 336_tohit | ne-bol ‘hit me’ | n e b o l + <s> <<‘>> </s> h i t + m e <s> <<’>> </s> |
+| abui-340_toknow-1 | abui | 340_toknow | nohiena ‘I know’ | n o h i e n a + <s> <<‘>> </s> I/ɪ + k n o w <s> <<’>> </s> |
+| abui-340_toknow-2 | abui | 340_toknow | ohiena ‘you know’ | o h i e n a + <s> <<‘>> </s> j o u + k n o w <s> <<’>> </s> |
+| abui-341_tolaugh-2 | abui | 341_tolaugh | na-lal ‘I laugh’ | n a l a l + <s> <<‘>> </s> I/ɪ + l a u g h <s> <<’>> </s> |
+| abui-342_tolive-1 | abui | 342_tolive | na-re ‘I live’ | n a r e + <s> <<‘>> </s> I/ɪ + l i v e <s> <<’>> </s> |
+| abui-351_tosee-2 | abui | 351_tosee | da nerei ‘he sees me’ | d a + n e r e i + <s> <<‘>> </s> h e + s e e s + m e <s> <<’>> </s> |
+| abui-365_towakeup-1 | abui | 365_towakeup | na-ruda   ‘I get up’ | n a r u d a + <s> <<‘>> </s> I/ɪ + g e t + u p <s> <<’>> </s> |
+| abui-53_child-2 | abui | 53_child | ne-wi ‘my child’ | n e w i + <s> <<‘>> </s> m j + tʃ h i l d <s> <<’>> </s> |
+| abui-98_fallfromabove-1 | abui | 98_fallfromabove | ditahai ‘fall from above’ | d i t a h a i + <s> <<‘>> </s> f a l l + f r o m + a b o v e <s> <<’>> </s> |
+| abui-98_fallfromabove-2 | abui | 98_fallfromabove | da-kuila ‘fall’ | d a k u i l a + <s> <<‘>> </s> f a l l <s> <<’>> </s> |
+| adang-163_lazy-2 | adang | 163_lazy | lahunu 'lazy' | l a h u n u + ʔ l a <s> <<z>> </s> j ʔ |
+| adang-72_dark-1 | adang | 72_dark | paraN 'dark  e.g. | p a r a ŋ + ʔ d a r k + e <s> <<.>> </s> g <s> <<.>> </s> |
+| blagar-11_ash-1 | blagar | 11_ash | məró ‘fireplace ash’ | m ə r o + <s> <<‘>> </s> f i r e p l a tʃ e + a s h <s> <<’>> </s> |
+| blagar-16_bamboo-1 | blagar | 16_bamboo | petuŋ ‘large wild bamboo’ | p e t u ŋ + <s> <<‘>> </s> l a r g e + w i l d + b a m b o o <s> <<’>> </s> |
+| blagar-16_bamboo-2 | blagar | 16_bamboo | mári ‘smooth wild bamboo’ | m a r i + <s> <<‘>> </s> s m o o t h + w i l d + b a m b o o <s> <<’>> </s> |
+| blagar-16_bamboo-3 | blagar | 16_bamboo | petuŋ vandaŋ ‘domestic bamboo’ | p e t u ŋ + v a n d a ŋ + <s> <<‘>> </s> d o m e s t i tʃ + b a m b o o <s> <<’>> </s> |
+| blagar-182_mango-1 | blagar | 182_mango | biyát ‘wild mango’ | b i j a t + <s> <<‘>> </s> w i l d + m a ŋ o <s> <<’>> </s> |
+| blagar-182_mango-2 | blagar | 182_mango | mampleʔ ‘domestic mango’ | m a m p l e ʔ + <s> <<‘>> </s> d o m e s t i tʃ + m a ŋ o <s> <<’>> </s> |
+| blagar-205_oldpeople-1 | blagar | 205_oldpeople | boma ‘old man' | b o m a + <s> <<‘>> </s> o l d + m a n ʔ |
+| blagar-206_oldwoman-1 | blagar | 206_oldwoman | meet ‘old woman’ | m e e t + <s> <<‘>> </s> o l d + w o m a n <s> <<’>> </s> |
+| blagar-211_onehundredthousand-1 | blagar | 211_onehundredthousand | salák ‘one million’ | s a l a k + <s> <<‘>> </s> o n e + m i l l i o n <s> <<’>> </s> |
+| blagar-21_bean-2 | blagar | 21_bean | tool butuŋ ‘peanut’ | t o o l + b u t u ŋ + <s> <<‘>> </s> p e a n u t <s> <<’>> </s> |
+| blagar-21_bean-3 | blagar | 21_bean | kaa ‘mung bean’ | k a a + <s> <<‘>> </s> m u ŋ + b e a n <s> <<’>> </s> |
+| blagar-221_rattan-1 | blagar | 221_rattan | líya ‘long rattan’ | l i j a + <s> <<‘>> </s> l o ŋ + r a t t a n <s> <<’>> </s> |
+| blagar-221_rattan-2 | blagar | 221_rattan | nəvár ‘short rattan’ | n ə v a r + <s> <<‘>> </s> s h o r t + r a t t a n <s> <<’>> </s> |
+| blagar-268_smooth-1 | blagar | 268_smooth | dúla ‘slippery’ | d u l a + <s> <<‘>> </s> s l i p p e r j <s> <<’>> </s> |
+| blagar-270_sorewound-1 | blagar | 270_sorewound | báta ‘old wound’ | b a t a + <s> <<‘>> </s> o l d + w o u n d <s> <<’>> </s> |
+| blagar-270_sorewound-2 | blagar | 270_sorewound | íbat ‘new wound’ | i b a t + <s> <<‘>> </s> n e w + w o u n d <s> <<’>> </s> |
+| blagar-321_tocut-1 | blagar | 321_tocut | parás ‘cut’ | p a r a s + <s> <<‘>> </s> tʃ u t <s> <<’>> </s> |
+| blagar-321_tocut-2 | blagar | 321_tocut | toniŋ ‘slice’ | t o n i ŋ + <s> <<‘>> </s> s l i tʃ e <s> <<’>> </s> |
+| blagar-323_todance-1 | blagar | 323_todance | dári ‘lego-lego’ | d a r i + <s> <<‘>> </s> l e g o l e g o <s> <<’>> </s> |
+| blagar-323_todance-2 | blagar | 323_todance | sarsumba ‘dance’ | s a r s u m b a + <s> <<‘>> </s> d a n tʃ e <s> <<’>> </s> |
+| blagar-350_tosay-1 | blagar | 350_tosay | tutuktutuk ‘chat’ | t u t u k t u t u k + <s> <<‘>> </s> tʃ h a t <s> <<’>> </s> |
+| blagar-354_tosmell-1 | blagar | 354_tosmell | mimiŋ ‘smell’ | m i m i ŋ + <s> <<‘>> </s> s m e l l <s> <<’>> </s> |
+| blagar-354_tosmell-2 | blagar | 354_tosmell | miku ‘kiss’ | m i k u + <s> <<‘>> </s> k i s s <s> <<’>> </s> |
+| blagar-392_word-1 | blagar | 392_word | hur ‘language’ | h u r + <s> <<‘>> </s> l a ŋ u a g e <s> <<’>> </s> |
+| blagar-397_youngpeople-1 | blagar | 397_youngpeople | kuar ‘young man’ | k u a r + <s> <<‘>> </s> j o u ŋ + m a n <s> <<’>> </s> |
+| blagar-397_youngpeople-2 | blagar | 397_youngpeople | jauŋ kenewiŋ ‘young woman’ | dʒ a u ŋ + k e n e w i ŋ + <s> <<‘>> </s> j o u ŋ + w o m a n <s> <<’>> </s> |
+| blagar-49_chaseawayexpel-2 | blagar | 49_chaseawayexpel | sika ‘sound to shoo a dog’ | s i k a + <s> <<‘>> </s> s o u n d + t o + s h o o + a + d o g <s> <<’>> </s> |
+| blagar-49_chaseawayexpel-3 | blagar | 49_chaseawayexpel | bura ‘sound to shoo a chicken’ | b u r a + <s> <<‘>> </s> s o u n d + t o + s h o o + a + tʃ h i tʃ k e n <s> <<’>> </s> |
+| blagar-49_chaseawayexpel-4 | blagar | 49_chaseawayexpel | sambe ‘sound to shoo a goat’ | s a m b e + <s> <<‘>> </s> s o u n d + t o + s h o o + a + g o a t <s> <<’>> </s> |
+| kamang-229_river-1 | kamang | 229_river | ili ma’tang | i l i + m a <s> <<’>> </s> t a ŋ |
+| kamang-98_fallfromabove-1 | kamang | 98_fallfromabove | mu’tan | m u <s> <<’>> </s> t a n |
+| klon-252_shark-1 | klon | 252_shark | be’ebi | b e <s> <<’>> </s> e b i |
+| klon-384_whale-1 | klon | 384_whale | be’ebi | b e <s> <<’>> </s> e b i |
+| klon-82_dolphin-1 | klon | 82_dolphin | be’ebi | b e <s> <<’>> </s> e b i |
+| protoalorpantar-337_tohold-1 | protoalorpantar | 337_tohold | *pVN2 | p V/ǝ ŋ <s> <<2>> </s> |
 
-(0 rows)
+(63 rows)
 
 
--- lexibank_robinsonap.py
+++ lexibank_robinsonap.py
@@ -1,179 +1,138 @@
 import re
+from pathlib import Path
 
+import attr
 from clldutils.misc import slug
-from clldutils.path import Path
-from clldutils.text import split_text, strip_brackets
-from pylexibank.dataset import Dataset as BaseDataset
-from pylexibank.util import getEvoBibAsBibtex
+# from clldutils.text import split_text, strip_brackets
+from pylexibank import Dataset as BaseDataset
+from pylexibank import Language as BaseLanguage
+from pylexibank import FormSpec
+
+
+gloss_quotes = re.compile(r"'.*?'", re.UNICODE)
+
+
+@attr.s
+class Language(BaseLanguage):
+    # the tokens used as language identifiers by Robinson and Holton
+    Token = attr.ib(default=None)
 
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "robinsonap"
-    DEST = ["AP_lexicon_coded.txt", "AP_lexicon.txt"]
+    language_class = Language
 
-    def cmd_install(self, **kw):
-        # read data from 'AP_lexicon_coded.txt' and collect it; self.raw has no
-        # DictReader method, so we need to emulate it (read the header to get field names, etc);
-        # given the file structure of double lines, we keep an internal account of
-        # which line we are in (form or cogid), in order to make the loop simple to
-        # understand
-        vocabulary = {}
-        rows = self.raw.read_tsv("AP_lexicon_coded.txt")
-        header = True
-        in_form = True  # switching from form/cogid
-        for row in rows:
-            if header:
-                fields = row
-                header = False
-                continue
+    form_spec = FormSpec(
+        brackets={"[": "]", "{": "}", "(": ")"},
+        separators=";/,",
+        missing_data=('?', '-', '', ''),
+        strip_inside_brackets=True
+    )
+    
+    def cmd_makecldf(self, args):
+        args.writer.add_sources()
 
-            # using the English gloss as key; replacing commas in the
-            # English gloss by slashes, in order to use CSV without quoting,
-            # and lowering
-            if in_form:
-                # build a dictionary of the forms for the entry
-                entry = {k: {"form": v} for k, v in zip(fields, row)}
+        languages = args.writer.add_languages(
+            lookup_factory='Token',
+        )
 
-                # remove non Alor-Pantar fields (keeping the English one as gloss)
-                [entry.pop(key) for key in ["Number", ""]]
+        concepts = args.writer.add_concepts(
+            id_factory=lambda c: c.id.split('-')[-1]+ '_' + slug(c.english),
+            lookup_factory="Name"
+        )
 
-                # fix/normalize the gloss
-                gloss = entry.pop("English")["form"]
-                gloss = gloss.replace(",", "/")
-                gloss = gloss.lower().strip()
+        # read data from AP_lexicon_coded.txt
+        seen = []
+        for f in ("AP_lexicon_coded.txt", "AP_lexicon.txt"):
+            for row in self.raw_dir.read_csv(f, dicts=True, delimiter="\t"):
+                concept = row['English'].lower().strip().replace(", ", "/")
+                # skip rows in AP_lexicon.txt that we've already seen
+                # in AP_lexicon_coded.txt
+                if f == 'AP_lexicon.txt' and concept in seen:
+                    continue
 
-                # append to vocabulary
-                vocabulary[gloss] = entry
+                # manually catch "chase away". There are two glosses for this:
+                #   367   "chase away"
+                #   368   "chase away, expel"
+                # ...-> 367 looks only partially complete and 368 contains all
+                # forms in 367, so ignore 367.
+                if row['English'] == 'chase away':
+                    continue
 
-                # next line is not forms (it is cogids)
-                in_form = False
-            else:
-                # build a dictionary of the cogids for the entry
-                entry = {k: v for k, v in zip(fields, row)}
+                if row['English']:
+                    # store lexicon IDs for the cognate row.
+                    lexicon_ids = {}
 
-                # remove non Alor-Pantar fields and update the data with the cogids
-                [entry.pop(key) for key in ["English", "Number", ""]]
+                    for lang in languages:
+                        assert concept in concepts, 'bad concept %s' % concept
 
-                # update the vocabulary/data
-                for lang, cogid in entry.items():
-                    vocabulary[gloss][lang]["cogid"] = cogid
+                        # preprocess value
+                        value = row[lang].replace("‘", "'").replace("’", "'")
+                        # remove the reconstruction mark (for proto-AP)
+                        value = value.lstrip("*")
+                        # strip glosses (between single quotes); 
+                        # strip_brackets() can't be used here because in 
+                        # case of unclosed brackets it strips away all the 
+                        # contents until the end of the string, which makes 
+                        # sense for parentheses and the like but can't be used 
+                        # here, as the single quote can be a glottal stop and 
+                        # as a word can (potentially) have more than one 
+                        # glottal stop (and this would strip all the sounds in 
+                        # the middle)
+                        if value.endswith("'"):
+                            value = gloss_quotes.sub("", value)
 
-                # next line is not cogids (it is forms)
-                in_form = True
+                        # remove leading & trailing spaces
+                        value = value.strip().lstrip()
 
-        # Read data from 'AP_lexicon.txt', collecting information for concepts that were
-        # manually excluded by the authors from 'AP_lexicon_coded.txt'
-        rows = self.raw.read_tsv("AP_lexicon.txt")
-        header = True
-        for row in rows:
-            if header:
-                fields = row
-                header = False
-                continue
+                        # if the stripped form starts and ends with a slash,
+                        # it is a leftover from a transcription, let's clean 
+                        # it (it could be done with the orthographic profile,
+                        # but this could hide errors in parsing multiple 
+                        # forms, and in any case this is more adequate as we 
+                        # get the correct value)
+                        if value.startswith("/") and value.endswith("/"):
+                            value = value[1:-1]
 
-            # build a dictionary of the forms for the entry
-            entry = {k: {"form": v} for k, v in zip(fields, row)}
-
-            # remove non Alor-Pantar fields (keeping the English one as gloss)
-            entry.pop("Number")
-
-            # fix/normalize the gloss
-            gloss = entry.pop("English")["form"]
-            gloss = gloss.replace(",", "/")
-            gloss = gloss.lower().strip()
-
-            # append to vocabulary if missing
-            if gloss not in vocabulary:
-                vocabulary[gloss] = entry
-
-        # remove problematic gloss
-        vocabulary.pop("chase away")
-        csid_map = {"_": 0}
-
-        with self.cldf as ds:
-            ds.add_sources()
-            ds.add_languages()
-            ds.add_concepts(id_factory=lambda c: slug(c.label))
-
-            # add forms
-            for concept in sorted(vocabulary):
-                for lang in sorted(vocabulary[concept]):
-                    # replace commas by slashes (so we don't need lots of
-                    # escaping and quoting) and correct bad entries;
-                    # lexibank `value` is actually `form` in the source
-                    value = vocabulary[concept][lang]["form"].replace(",", "/")
-                    if value in self.lexemes:
-                        value = self.lexemes[value]
-
-                    for form in split_text(value, separators="/"):
-                        # replace single quote characters
-                        form = form.replace("‘", "'")
-                        form = form.replace("’", "'")
-
-                        # remove the reconstruction mark (for proto-alor-pantar)
-                        if form.startswith("*"):
-                            form = form[1:]
-
-                        # strip brackets, parentheses, etc.
-                        form = strip_brackets(form, brackets={"[": "]", "{": "}", "(": ")"})
-
-                        # strip glosses (between single quotes); strip_brackets() can't be
-                        # used here because in case of unclosed brackets it strips away
-                        # all the contents until the end of the string, which makes sense
-                        # for parentheses and the like but can't be used here, as the
-                        # single quote can be a glottal stop and as a word can (potentially)
-                        # have more than one glottal stop (and this would strip all the
-                        # sounds in the middle)
-                        if form.endswith("'"):
-                            gloss_quotes = re.compile(r"'.*?'", re.UNICODE)
-                            form = re.sub(gloss_quotes, "", form)
-
-                        # remove multiple spaces, plurs leading&trailing spaces
-                        form = re.sub(r"\s+", " ", form).strip()
-
-                        # if the stripped form starts and ends with a slash, it is a
-                        # leftover from a transcription, let's clean it (it could be
-                        # done with the orthographic profile, but this could hide errors
-                        # in parsing multiple forms, and in any case this is more adequate
-                        # as we get the correct value)
-                        if form.startswith("/") and form.endswith("/"):
-                            form = form[1:-1]
-
-                        # skip over empty of invalid forms
-                        if not form:
-                            continue
-                        if form in ["-", "--"]:
-                            continue
-
-                        for row in ds.add_lexemes(
-                            Local_ID=lang,
-                            Language_ID=lang,
-                            Parameter_ID=slug(concept),
-                            Value=form,
+                        lex = args.writer.add_forms_from_value(
+                            Language_ID=languages[lang],
+                            Parameter_ID=concepts[concept],
+                            Value=value,
                             Source=["Robinson2012"],
-                        ):
-                            if "cogid" in vocabulary[concept][lang]:
-                                csid = "%s-%s" % (slug(concept), vocabulary[concept][lang]["cogid"])
-                                if csid not in csid_map:
-                                    csid_map[csid] = max(csid_map.values()) + 1
-                                ds.add_cognate(
-                                    lexeme=row,
-                                    Cognateset_ID=csid_map[csid],
-                                    Source=["Robinson2012"],
-                                    Alignment_Source="List2014e",
+                        )
+                        if len(lex) >= 1:
+                            # it looks like only the first lexemes of combined 
+                            # forms have cognates, so only add the first one.
+                            lexicon_ids[lang] = lex[0]
+
+                    seen.append(concept)
+
+                else:  # cognates...
+                    lastword = seen[-1]  # find the last word..
+                    for lang in languages:
+                        # find lexical ids belonging to this language & gloss.
+                        lex = lexicon_ids.get(lang)
+                        if lex and row[lang]:
+                            if int(row[lang]) not in range(0, 12):
+                                raise ValueError(
+                                    "Invalid cognate id: %s" % row[lang]
                                 )
+                            args.writer.add_cognate(
+                                lexeme=lex,
+                                Cognateset_ID="%s-%s" % (concepts[lastword], row[lang]),
+                                Source=["Robinson2012"]
+                            )
 
 
     def cmd_download(self, **kw):
-        if not self.raw.exists():
-            self.raw.mkdir()
-        self.raw.download_and_unpack(
+        if not self.raw_dir.exists():
+            self.raw_dir.mkdir()
+        files = ["AP_lexicon_coded.txt", "AP_lexicon.txt"]
+        self.raw_dir.download_and_unpack(
             "http://booksandjournals.brillonline.com/upload/"
             "robinson_10.116322105832-20120201.zip?itemId="
             "/content/journals/10.1163/22105832-20120201&mimeType=application/octet-stream",
-            *[Path(f) for f in self.DEST],
+            *[Path(f) for f in files],
             log=self.log
         )
-
-        self.raw.write("sources.bib", getEvoBibAsBibtex("Robinson2012", "List2014e", **kw))
