e229c153795ed44dc3eb1a390359c97ce0402777
chrzyki <christoph@functor-argument.net>
2019-11-11 10:21:13 +0100

    Minor refactorings, re-generated CLDF, release prep

--- lexibank_robinsonap.py
+++ lexibank_robinsonap.py
@@ -2,14 +2,13 @@ from pathlib import Path
 
 import attr
 from clldutils.misc import slug
-# from clldutils.text import split_text, strip_brackets
 from pylexibank import Dataset as BaseDataset
 from pylexibank import Language as BaseLanguage
 from pylexibank import FormSpec
 
 
 @attr.s
-class Language(BaseLanguage):
+class CustomLanguage(BaseLanguage):
     # the tokens used as language identifiers by Robinson and Holton
     Token = attr.ib(default=None)
 
@@ -17,31 +16,28 @@ class Language(BaseLanguage):
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "robinsonap"
-    language_class = Language
+    language_class = CustomLanguage
 
     form_spec = FormSpec(
         brackets={"[": "]", "{": "}", "(": ")", "‘": "’"},
         separators=";/,",
-        missing_data=('?', '-', '', ''),
-        strip_inside_brackets=True
+        missing_data=("?", "-", "", ""),
+        strip_inside_brackets=True,
     )
-    
+
     def cmd_makecldf(self, args):
         args.writer.add_sources()
 
-        languages = args.writer.add_languages(
-            lookup_factory='Token',
-        )
+        languages = args.writer.add_languages(lookup_factory="Token")
 
         concepts = args.writer.add_concepts(
-            id_factory=lambda c: c.id.split('-')[-1]+ '_' + slug(c.english),
-            lookup_factory="Name"
+            id_factory=lambda c: c.id.split("-")[-1] + "_" + slug(c.english), lookup_factory="Name"
         )
 
         seen = []
         for f in ("AP_lexicon_coded.txt", "AP_lexicon.txt"):
             for row in self.raw_dir.read_csv(f, dicts=True, delimiter="\t"):
-                concept = row['English'].lower().strip().replace(", ", "/")
+                concept = row["English"].lower().strip().replace(", ", "/")
                 # skip rows in AP_lexicon.txt that we've already seen
                 # in AP_lexicon_coded.txt
                 # Note that there are duplicate rows in across both files, and *within*
@@ -54,17 +50,17 @@ class Dataset(BaseDataset):
                 #   368   "chase away, expel"
                 # ...-> 367 looks only partially complete and 368 contains all
                 # forms in 367, so ignore 367.
-                if row['English'] == 'chase away':
+                if row["English"] == "chase away":
                     continue
 
-                if row['English']:
+                if row["English"]:
                     # store lexicon IDs for the cognate row.
                     lexicon_ids = {}
 
                     for lang in languages:
-                        assert concept in concepts, 'bad concept %s' % concept
+                        assert concept in concepts, "bad concept %s" % concept
                         value = row[lang]
-                        
+
                         # preprocess value
                         # remove the reconstruction mark (for proto-AP)
                         value = value.lstrip("*")
@@ -73,10 +69,10 @@ class Dataset(BaseDataset):
                         value = value.strip().lstrip()
 
                         # if the stripped form starts and ends with a slash,
-                        # it is a leftover from a transcription, let's clean 
+                        # it is a leftover from a transcription, let's clean
                         # it (it could be done with the orthographic profile,
-                        # but this could hide errors in parsing multiple 
-                        # forms, and in any case this is more adequate as we 
+                        # but this could hide errors in parsing multiple
+                        # forms, and in any case this is more adequate as we
                         # get the correct value)
                         if value.startswith("/") and value.endswith("/"):
                             value = value[1:-1]
@@ -88,7 +84,7 @@ class Dataset(BaseDataset):
                             Source=["Robinson2012"],
                         )
                         if len(lex) >= 1:
-                            # it looks like only the first lexemes of combined 
+                            # it looks like only the first lexemes of combined
                             # forms have cognates, so only add the first one.
                             lexicon_ids[lang] = lex[0]
 
@@ -101,15 +97,12 @@ class Dataset(BaseDataset):
                         lex = lexicon_ids.get(lang)
                         if lex and row[lang]:
                             if int(row[lang]) not in range(0, 12):
-                                raise ValueError(
-                                    "Invalid cognate id: %s" % row[lang]
-                                )
+                                raise ValueError("Invalid cognate id: %s" % row[lang])
                             args.writer.add_cognate(
                                 lexeme=lex,
                                 Cognateset_ID="%s-%s" % (concepts[lastword], row[lang]),
-                                Source=["Robinson2012"]
+                                Source=["Robinson2012"],
                             )
-
 
     def cmd_download(self, **kw):
         if not self.raw_dir.exists():
@@ -119,6 +112,5 @@ class Dataset(BaseDataset):
             "http://booksandjournals.brillonline.com/upload/"
             "robinson_10.116322105832-20120201.zip?itemId="
             "/content/journals/10.1163/22105832-20120201&mimeType=application/octet-stream",
-            *[Path(f) for f in files],
-            log=self.log
+            *[Path(f) for f in files]
         )
--- setup.py
+++ setup.py
@@ -2,30 +2,19 @@ from setuptools import setup
 import json
 
 
-with open('metadata.json') as fp:
+with open("metadata.json") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_robinsonap',
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_robinsonap'],
+    name="lexibank_robinsonap",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_robinsonap"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'robinsonap=lexibank_robinsonap:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=2.0',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
+    entry_points={"lexibank.dataset": ["robinsonap=lexibank_robinsonap:Dataset"]},
+    install_requires=["pylexibank>=2.1"],
+    extras_require={"test": ["pytest-cldf"]},
 )
-
